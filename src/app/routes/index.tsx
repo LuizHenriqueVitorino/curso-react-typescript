91af51458c30b0f1f6508b8c274c86fc9ba9d352
Miguel Santos <miguel.santoss085@gmail.com>
2022-10-06 17:04:20 -0300

    inicio da organizacao da estrutura de paginas

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,14 @@
+import { BrouserRouter } from "react-router-dom";
+
+// export const Routes = () =>{
+//     <Browser>
+//         {/* switch (key) {
+//             case value:
+                
+//                 break;
+        
+//             default:
+//                 break; */}
+//         }
+//     </Browser>
+// }
